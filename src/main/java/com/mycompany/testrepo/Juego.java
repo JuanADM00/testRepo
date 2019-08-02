c0cb1e0b3e3fa5c205641c2b2ed3eead08c0e6ae
informatica <informatica@BGAI202-03.bga.upb>
2019-08-02 15:28:29 -0500

    Se implementó el contador.

--- src/main/java/com/mycompany/testrepo/Juego.java
+++ src/main/java/com/mycompany/testrepo/Juego.java
@@ -14,16 +14,23 @@ import java.util.Scanner;
  */
 public class Juego {
     public static void main(String[] args) {
-        System.out.println("Dame un número entre uno y veinte");
+        
+        int numeroaleatorio, numero;
         Scanner teclado = new Scanner(System.in);
-        int numero = Integer.parseInt( teclado.nextLine());
         Random r = new Random();
-        int numeroaleatorio = r.nextInt(20)+1;
+        int contador = 0;
+        do{
+        System.out.println("Dame un número entre uno y veinte");
+        numero = Integer.parseInt( teclado.nextLine());
+        numeroaleatorio = r.nextInt(20)+1;
         if (numero == numeroaleatorio){
             System.err.println("Ganaste con el número "+numero);
         }
         else{
             System.err.println("Pailas, el número correcto era "+numeroaleatorio+" y no "+numero);
         }
+        contador++;
+        }
+        while (numero != numeroaleatorio);
     }
 }
